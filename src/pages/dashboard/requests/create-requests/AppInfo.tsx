4beb219c3618b587871e17dce93f042fa3b9f64f
Susu-spec <suleimansuwaibat@gmail.com>
2024-08-09 16:26:05 +0100

    add appInfo, fix build error

--- src/pages/dashboard/requests/create-requests/AppInfo.tsx
+++ src/pages/dashboard/requests/create-requests/AppInfo.tsx
@@ -2,26 +2,54 @@
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { Form } from "@/components/ui/form";
+import { Form, FormControl, FormField, FormItem, FormLabel } from "@/components/ui/form";
 // import FormCheckBox from "@/components/custom/FormCheckBox";
 // import { useMediaQuery } from "react-responsive";
 import { useRequestsStore } from "@/context/RequestFormStore";
+import { Checkbox } from "@radix-ui/react-checkbox";
 
 
-const formSchema = z.object({
+const formSchema = z.
+object({
     checkbox: z
-    .string()
-
+    .object({
+        key: z.object({
+            id: z.string(),
+            label: z.string(),
+            value: z.boolean()
+        })
+    })
 })
-type UnexpectedObject = {
-    [key: string]: any;
-  };
+
+const questions = [
+    {
+    key: {
+        id: "1",
+        label: "Why",
+        value: true,
+    }
+},
+    {
+    key: {
+        id: "2",
+        label: "How",
+        value: true,
+    }
+},
+    {
+    key: {
+        id: "3",
+        label: "What",
+        value: true,
+    }
+},
+]
+
 
 type Props = {
-    handleNext: Function,
-
-    checkBoxInfo?: UnexpectedObject
+    handleNext: Function
 }
+
 
 export function AppInfo({ handleNext}: Props) {
     // const isMobile = useMediaQuery({query: 'min-width: 768px'});
@@ -29,20 +57,19 @@ export function AppInfo({ handleNext}: Props) {
     const { data, setData } = useRequestsStore();
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
-    })
+    });
+    const { control } = form;
 
     // const { register, reset, formState: { isValid }} = form;
     // const [checkbox, setCheckbox] = useState<string>("");
     // const [checkboxArray, setCheckboxArray] = useState<string[]>([]);
 
-    const checkboxArray = [""];
-
-    function onSubmit() {
+    function onSubmit(values: z.infer<typeof formSchema>) {
         try {
             setData({
                 requestsDetails: {
                     ...data.requestsDetails,
-                    checkbox: checkboxArray
+                    checkbox: values.checkbox
                 }
             });
             handleNext();
@@ -59,8 +86,46 @@ export function AppInfo({ handleNext}: Props) {
             <div className="md:w-3/5 w-full max-w-[358px] md:max-w-[526px] mx-auto my-0">
                 <Form {...form}>
                     <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col">
-                        {/* <FormCheckBox 
-                            {...register("")/> */}
+                        <FormField
+                            control={control}
+                            name="checkbox"
+                            render={() => (
+                                <FormItem>
+                                    <div>
+                                        {questions.map(({ key }) => (
+                                            <FormField
+                                                key={key.id}
+                                                control={control}
+                                                name="checkbox"
+                                                render={({ field }) => {
+                                                    // const { value, onChange } = field;
+                                                    return (
+                                                        <FormItem 
+                                                            key={key.id}
+                                                            >
+                                                             <FormLabel>{key.label}</FormLabel>
+                                                                <FormControl>
+                                                                <Checkbox
+                                                                    checked={key.value === true}
+                                                                    onCheckedChange={field.onChange}
+                                                                    >Yes</Checkbox>
+                                                                </FormControl>
+                                                                <FormControl>
+                                                                <Checkbox
+                                                                    checked={key.value === false}
+                                                                    onCheckedChange={field.onChange}
+                                                                >No</Checkbox>
+                                                                </FormControl>
+                                                            </FormItem>
+                                                    )
+                                                }}>
+
+                                            </FormField>
+                                        ))}
+                                    </div>
+                                </FormItem>
+                            )}
+                            />
                     </form>
 
                 </Form>
