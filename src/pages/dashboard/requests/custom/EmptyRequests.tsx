006ca4961def0bf67c4e7545482c42068f07c84d
Susu-spec <suleimansuwaibat@gmail.com>
2024-08-15 20:42:44 +0100

    modify buttons

--- src/pages/dashboard/requests/custom/EmptyRequests.tsx
+++ src/pages/dashboard/requests/custom/EmptyRequests.tsx
@@ -30,7 +30,7 @@ export default function EmptyRequests({handleNext}: Props) {
                 <div className="flex flex-col gap-6 w-full max-w-[37rem] text-center">
                     <h1 className="text-[1.625rem] leading-8 font-bold">Research ethics approval made easy</h1>
                     <p>Enjoy efficiency, transparency, and accountability while eliminating material losses and communication delays in the review process</p>
-                    <Button onClick={() => {handleFunc()}} className="flex gap-4 items-center justify-center py-3 px-6"><span><GoogleDoc /></span>Request Ethical Approval</Button>
+                    <Button onClick={() => {handleFunc()}} className="flex gap-4 items-center justify-center py-3 px-6 w-full max-w-[18rem] mx-auto"><span><GoogleDoc /></span>Request Ethical Approval</Button>
                 </div>
             </div>
         </>   
