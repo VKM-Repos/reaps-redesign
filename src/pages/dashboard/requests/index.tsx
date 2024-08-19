99d8f7da7294ea7e0fd36f6f11156c77c44da069
Susu-spec <suleimansuwaibat@gmail.com>
2024-08-19 17:05:20 +0100

    add spacing

--- src/pages/dashboard/requests/index.tsx
+++ src/pages/dashboard/requests/index.tsx
@@ -66,7 +66,7 @@ const CreateRequests = () => {
 
     }
     return (
-        <div className="flex flex-col gap-[1.25rem]">
+        <div className="flex flex-col gap-[1.25rem] mb-20">
             <div className="flex flex-col md:flex-row gap-5 md:gap-auto justify-between md:items-center mx-auto w-full">
                 <h1 className="text-[1.875rem] font-bold">Requests</h1>
             </div>
