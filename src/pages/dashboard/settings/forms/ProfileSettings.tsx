898e95ff78ecd6ac030373d13cbfb6175fa0bbca
Susu-spec <suleimansuwaibat@gmail.com>
2024-09-23 10:26:30 +0100

    update countries field

--- src/pages/dashboard/settings/forms/ProfileSettings.tsx
+++ src/pages/dashboard/settings/forms/ProfileSettings.tsx
@@ -2,20 +2,29 @@ import { useForm } from "react-hook-form";
 import FormInput from "@/components/custom/FormInput";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Form } from "@/components/ui/form";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { z } from "zod";
 import { useOnboardingFormStore } from "@/store/CreateOnboardingFormStore";
 import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
-import countryCodes from "@/lib/data/CountryCodes.json"
+import { countries, CountryListItemType } from "country-list-json";
+import countryFlags from "@/lib/data/countries.json"
 import { Label } from "@/components/ui/label";
 import CustomFormField, { FormFieldType } from "@/components/custom/CustomFormField";
 import Loader from "@/components/custom/Loader";
 
 
+type FlagData = { 
+    url: string; 
+    alpha3: string; 
+    name: string; 
+    file_url: string; 
+    license: string; 
+}
+
 
 export const ProfileSettings = ({ onSave }: { onSave: () => void }) => {
-    const [dialCode, setDialCode] = useState("+234");
+    const [dialCode, setDialCode] = useState("+93");
     const [loading, setLoader] = useState(false);
     const formSchema = z.object({
         firstName: z
@@ -36,7 +45,28 @@ export const ProfileSettings = ({ onSave }: { onSave: () => void }) => {
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
       });
-      const { register, formState: { isValid }, reset} = form;
+    const { register, formState: { isValid }, reset} = form;
+    const [flags, setFlags] = useState<FlagData[]>([]);
+    const [countriesData, setCountries] = useState<CountryListItemType[]>([])
+
+    useEffect(() => {
+        setCountries(countries);
+        setFlags(countryFlags);
+    }, []);
+
+
+
+    const combinedData = countriesData
+        .filter(country => flags.some(f => f.name === country.name))
+        .map(country => {
+        const flag = flags.find(f => f.name === country.name);
+        return {
+            ...country,
+            flag: flag?.file_url || ''
+        };
+    });
+
+
 
 
     function onSubmit(values: z.infer<typeof formSchema>) {
@@ -93,36 +123,41 @@ export const ProfileSettings = ({ onSave }: { onSave: () => void }) => {
                     <div className="flex gap-2">
                         <div className="flex flex-col text-xs mt-2">
                             <Select onValueChange={(value: string) => {
-                                    const selectedCountry: any = countryCodes.find(country => country.name === value);
+                                    const selectedCountry: any = combinedData.find(country => country.name === value);
                                     if (selectedCountry) {
                                     setDialCode(selectedCountry.dial_code);
                                     }
                                 }}>
                                 <Label className="font-md">Country Code</Label>
-                                <SelectTrigger className="max-w-28 mt-2">
-                                    <SelectValue placeholder={dialCode} />
+                                <SelectTrigger className="max-w-32 mt-2">
+                                    <SelectValue placeholder={dialCode}>{dialCode}</SelectValue>
                                 </SelectTrigger>
                                 <SelectContent>
                                     <SelectGroup> 
                                         <SelectLabel>Country Codes</SelectLabel>
-                                        {countryCodes && countryCodes.map((country) => (
-                                                <SelectItem
-                                                    value={country.name}
+                                        {combinedData && combinedData.map((country) => (
+                                                <SelectItem 
+                                                key={country.name} value={country.name}
                                                 >
-                                                    {country.dial_code}
+                                                    <div className="flex gap-4 items-center justify-center">
+                                                        <span><img src={country.flag} height="24px" width="24px"/></span>
+                                                        <span>{country.name}</span>
+                                                        <span>{country.dial_code}</span>
+                                                    </div>
                                                 </SelectItem>
                                         ))}
                                     </SelectGroup>
                                 </SelectContent>
                             </Select>
                         </div>
-                        
                         <div className="w-full">
                             <FormInput
                                 label="Phone number"
+                                type="number"
                                 {...register("phoneNumber", {
                                 required: "This field is required",
-                                })}   
+                                })}  
+                                className="no-spinner"
                             />
                         </div>  
                     </div>
