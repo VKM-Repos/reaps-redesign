73d6b898c38181a435fe2b4310f62131a579dfdc
Susu-spec <suleimansuwaibat@gmail.com>
2024-09-30 12:40:44 +0100

    switch category button

--- src/pages/dashboard/pricing/index.tsx
+++ src/pages/dashboard/pricing/index.tsx
@@ -5,11 +5,13 @@ import { useState } from "react";
 import ViewTransactions from "./view-transactions";
 import { useNavigate } from "react-router-dom";
 import Loader from "@/components/custom/Loader";
+import useUserStore from "@/store/user-store";
 
 export default function Pricing() {
     const [ showTransactions, setShowTransactions ] = useState(false);
     const [ loading, setLoading ] = useState(false);
     const navigate = useNavigate();
+    const { activeRole } = useUserStore(); 
 
     const handleFunc = () => {
         setShowTransactions(true);
@@ -30,17 +32,17 @@ export default function Pricing() {
             <div className="flex flex-col gap-[1.5rem] mb-20">
                 {showTransactions ?
                     <ViewTransactions setShowTransactions={setShowTransactions}/> :
-                <>
+                <div className="flex flex-col gap-14 items-center">
                     <div className="flex flex-col gap-[1.25rem] mx-auto w-full">
                         <div className="flex flex-col md:flex-row gap-5 md:gap-auto justify-between md:items-center">
                             <h1 className="text-[1.875rem] font-bold">Pricing</h1>
-                            <p onClick={handleFunc}><a className="text-sm text-primary font-semibold underline">View your transactions</a></p>
+                            <p onClick={handleFunc}><a className="text-sm text-[#192C8A]  font-semibold underline">View your transactions</a></p>
                         </div>
                         <p className="text-sm text-[#454745]">The pricing page helps you to get information on what you will be charged based on your category as a researcher.</p>
-                        <Button onClick={newCategory} className="flex gap-4 items-center justify-center py-3 px-6 w-full max-w-[10rem]">New Category</Button>
+                            {activeRole === "INSTITUTION_ADMIN" && <Button onClick={newCategory} className="flex gap-4 items-center justify-center py-3 px-6 w-full max-w-[10rem]">New Category</Button>}
                     </div>
                     <CategoryTable categories={categories}/>
-                </>
+                </div>
             }     
             </div>
         </>
