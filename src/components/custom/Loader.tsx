63e6e6a405647b256cf71f9a86d56b62fa1138e0
Susu-spec <suleimansuwaibat@gmail.com>
2024-11-04 13:51:42 +0100

    update to deploy

--- src/components/custom/Loader.tsx
+++ src/components/custom/Loader.tsx
@@ -4,7 +4,7 @@ export default function Loader() {
     return (
         <div className="fixed inset-0 z-[9999] w-screen h-screen bg-[#000000]/30 backdrop-blur-[6px] w-full flex justify-center items-center">
             <div className="relative">
-                <LoadingIcon />
+                <LoadingIcon  />
             </div>
         </div>
     )
