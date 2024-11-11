9efa01bc294cc3e276d9e1c0274bf46442dc45db
Susu-spec <suleimansuwaibat@gmail.com>
2024-11-11 12:44:21 +0100

    update mobile navbar for admin

--- src/components/custom/MobileNavBar.tsx
+++ src/components/custom/MobileNavBar.tsx
@@ -2,81 +2,88 @@ import { useSideBar } from "@/context/SideBarContext";
 import { Link, useLocation } from "react-router-dom";
 import ArrowRight from "./Icons/ArrowRight";
 import useUserStore from "@/store/user-store";
+import { useEffect, useState } from "react";
 
 
-export default function MobileNavBar() {
-    const { pathname }= useLocation();
+export default function MobileNavbar() {
+    const { pathname } = useLocation();
     const { links } = useSideBar();
     const { activeRole } = useUserStore();
-    
+    const [activeLabel, setActiveLabel ] = useState<string | null>();
+
+    const handleLinkClick = (label: string) => {
+        setActiveLabel(label);
+    };
+
+    // handle cases for when url is navigated in ways other than clicking the link
+    useEffect(() => {
+        if (activeRole === 'admin') {
+            if (pathname === '/requests') {
+                setActiveLabel('Requests')
+            } else if (pathname === '/institution') {
+                setActiveLabel('Institution')
+            }
+        }
+    }, [activeRole, pathname])
 
     return (
         <>
-        {links.map(({label, subLinks }) => activeRole === 'admin' && 
-                activeRole === 'admin' &&
-                (label === 'Requests' || label === 'Institution') ? (
-                    <>
-                        {/* <div className="hover:text-black flex space-x-4 px-2 items-center justify-between z-50">
+            {activeRole === 'admin' && activeLabel && (pathname === '/requests' || pathname === '/institution') &&
+                links.map(({ label, subLinks, path }) => 
+                    activeLabel === label && (pathname === path || pathname === path) && subLinks ? (
+                        <div key={label} className="mt-14">
+                            <NavComponent subLinks={subLinks} />
+                        </div>
+                    ) : null
+                )
+            }
+            <div className="w-full fixed bottom-0 flex flex-col justify-center bg-white min-h-[76px] px-3 py-4">
+                <ul className="flex items-center justify-between px-3 py-4">
+                    {links.map(({ path, icon, label }) => (
+                        <Link
+                            key={label}
+                            to={path}
+                            onClick={() => handleLinkClick(label)}
+                            className={`${
+                                path.includes(pathname) ? 'text-black' : 'text-black/30'
+                            } hover:text-black flex space-x-4 px-2 items-center justify-between`}
+                        >
                             <span>{icon}</span>
                             {path.includes(pathname) && <span>{label}</span>}
-                        </div> */}
-                        {subLinks && 
-                            <NavComponent subLinks={subLinks} />
-                        }  
-                    </>
-                )  
-                : 
-               (
-                <div className="w-full fixed bottom-0 flex flex-col justify-center bg-white min-h-[76px] px-3 py-4 ">
-                    <ul className="flex  items-center justify-between px-3 py-4">
-                        {links.map(({  path, icon, label }) => 
-                        (
-                            <Link to={path} className={`${path.includes(pathname) ? 'text-black' : 'text-black/30'} hover:text-black flex space-x-4 px-2 items-center justify-between z-50`}>
-                                <span>{icon}</span>
-                                {path.includes(pathname) && <span>{label}</span>}
-                            </Link>
-                        ))}
-                    </ul>
-                </div>
-               ) 
-            )}
-        
+                        </Link>
+                    ))}
+                </ul>
+            </div>
         </>
-        
-    )
-}
-// if role is admin and if path is /requests or /institution,
-// show component for navigation
+    );
+};
 
-
-export const NavComponent = ({ subLinks }: { subLinks: any[]}) => {
+const NavComponent = ({ subLinks }: { subLinks: any[] }) => {
     const { pathname } = useLocation();
 
     return (
         <div className="h-full w-full">
-            <div className="px-6 py-1">
+            <div className="px-6 py-1 w-full">
                 <div className="w-full flex flex-col gap-4">
-                    {subLinks.map(({ path, icon, label}) => (
+                    {subLinks.map(({ path, icon, label }) => (
                         <Link
-                        key={label}
-                        to={path}
-                        className={`${
-                          path.includes(pathname)
-                            ? 'text-black'
-                            : 'text-black/30'
-                        } hover:text-black flex justify-start `}
-                      >
-                        <span className="py-3 px-4">
-                            <span>{icon}</span>
-                            <span>{label}</span>
-                        </span>
-                        <span className="text-center">
-                            <ArrowRight />
-                        </span>
-                      </Link>
+                            key={label}
+                            to={path}
+                            className={`${
+                                path === pathname ? 'text-black' : 'text-black/30'
+                            } hover:text-black flex justify-between items-center w-full `}
+                        >
+                            <span className="py-3 px-4 flex gap-4">
+                                <span>{icon}</span>
+                                <span>{label}</span>
+                            </span>
+                            <span className="text-center">
+                                <ArrowRight />
+                            </span>
+                        </Link>
                     ))}
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
--- src/pages/dashboard/requests/index.tsx
+++ src/pages/dashboard/requests/index.tsx
@@ -57,79 +57,81 @@ export default function Requests() {
 
   return (
     <>
-      {loading && <Loader />}
-      <div className="flex flex-col gap-12 mb-20">
-        <div className="flex flex-col md:flex-row gap-5 md:gap-auto justify-between md:items-center mx-auto w-full">
-          <PageTitle title="Requests" />
-          {tableData.length > 0 && (
-            <Button
-              onClick={handleFunc}
-              className="flex gap-4 items-center justify-center py-3 px-6 max-w-[16.75rem]"
-            >
-              <span>
-                <GoogleDoc />
-              </span>
-              Request Ethical Approval
-            </Button>
-          )}
-        </div>
-        {/* tab */}
-        {tableData && tableData.length > 0 ? (
-          <div className="flex flex-col gap-4">
-            <div className="flex items-center justify-between">
-            <SeachFilter statuses={statuses} activeTab={activeTab} setLoading={setLoading} setShowStatuses={setShowStatuses} setAppliedStatuses={setAppliedStatuses}/>
-              <div className="lg:flex items-center gap-1 hidden">
-                <span>
-                  <a href="" className="font-semibold underline text-black">
-                    The approval process
-                  </a>
-                </span>
-                <span>
-                  <LinkIcon />
-                </span>
-              </div>
-            </div>
-            {isMobile && showStatuses && appliedStatuses.length !== 0 && (
-              <div className="flex flex-wrap justify-center items-center p-4 gap-3">
-                {appliedStatuses.map((status) => (
-                  <div className="py-2 px-3 border border-[#0C0C0F29] rounded-[0.625rem] flex items-center justify-start gap-2 w-full max-w-fit">
-                    <span className="w-[6px] h-[5px] bg-[#FFD13A] rounded-full"></span>
-                    <span className="text-xs font-semibold text-[#0C0D0F] w-full min-w-fit flex text-wrap">
-                      {status}
-                    </span>
-                    <span onClick={() => deleteStatusUpdate(status)}>
-                      <X size={10}  />
-                    </span>
-                  </div>
-                ))}
-              </div>
-            )}
-            {/* change tables to completed state  */}
-            {activeRole === 'reviewer' ? (
-              <Tabs
-                defaultValue="request table"
-                onValueChange={(val) => setActiveTab(val)}
+    {loading && <Loader />}
+    {activeRole !== 'admin' &&
+        <div className="flex flex-col gap-12 mb-20">
+          <div className="flex flex-col md:flex-row gap-5 md:gap-auto justify-between md:items-center mx-auto w-full">
+            <PageTitle title="Requests" />
+            {tableData.length > 0 && (
+              <Button
+                onClick={handleFunc}
+                className="flex gap-4 items-center justify-center py-3 px-6 max-w-[16.75rem]"
               >
-                <TabsList className="border-b-[1.5px] w-full px-3">
-                  <TabsTrigger value="request table">My request</TabsTrigger>
-                  <TabsTrigger value="review table">Review request</TabsTrigger>
-                </TabsList>
-
-                <TabsContent value="request table">
-                  <TableRequests tableData={tableData} />
-                </TabsContent>
-                <TabsContent value="review table">
-                  <TableReview reviewTableData={reviewTableData} />
-                </TabsContent>
-              </Tabs>
-            ) : (
-              <TableRequests tableData={tableData} />
+                <span>
+                  <GoogleDoc />
+                </span>
+                Request Ethical Approval
+              </Button>
             )}
           </div>
-        ) : (
-          <EmptyRequests />
-        )}
-      </div>
+          {/* tab */}
+          {tableData && tableData.length > 0 ? (
+            <div className="flex flex-col gap-4">
+              <div className="flex items-center justify-between">
+              <SeachFilter statuses={statuses} activeTab={activeTab} setLoading={setLoading} setShowStatuses={setShowStatuses} setAppliedStatuses={setAppliedStatuses}/>
+                <div className="lg:flex items-center gap-1 hidden">
+                  <span>
+                    <a href="" className="font-semibold underline text-black">
+                      The approval process
+                    </a>
+                  </span>
+                  <span>
+                    <LinkIcon />
+                  </span>
+                </div>
+              </div>
+              {isMobile && showStatuses && appliedStatuses.length !== 0 && (
+                <div className="flex flex-wrap justify-center items-center p-4 gap-3">
+                  {appliedStatuses.map((status) => (
+                    <div className="py-2 px-3 border border-[#0C0C0F29] rounded-[0.625rem] flex items-center justify-start gap-2 w-full max-w-fit">
+                      <span className="w-[6px] h-[5px] bg-[#FFD13A] rounded-full"></span>
+                      <span className="text-xs font-semibold text-[#0C0D0F] w-full min-w-fit flex text-wrap">
+                        {status}
+                      </span>
+                      <span onClick={() => deleteStatusUpdate(status)}>
+                        <X size={10}  />
+                      </span>
+                    </div>
+                  ))}
+                </div>
+              )}
+              {/* change tables to completed state  */}
+              {activeRole === 'reviewer' ? (
+                <Tabs
+                  defaultValue="request table"
+                  onValueChange={(val) => setActiveTab(val)}
+                >
+                  <TabsList className="border-b-[1.5px] w-full px-3">
+                    <TabsTrigger value="request table">My request</TabsTrigger>
+                    <TabsTrigger value="review table">Review request</TabsTrigger>
+                  </TabsList>
+
+                  <TabsContent value="request table">
+                    <TableRequests tableData={tableData} />
+                  </TabsContent>
+                  <TabsContent value="review table">
+                    <TableReview reviewTableData={reviewTableData} />
+                  </TabsContent>
+                </Tabs>
+              ) : (
+                <TableRequests tableData={tableData} />
+              )}
+            </div>
+          ) : (
+            <EmptyRequests />
+          )}
+        </div>
+      }
     </>
   );
 }
