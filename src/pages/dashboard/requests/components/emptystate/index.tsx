1c26d789637850dd74a3b4b1440d266ad01678f7
Susu-spec <suleimansuwaibat@gmail.com>
2024-11-29 05:56:27 +0100

    move files

--- src/pages/dashboard/requests/components/emptystate/index.tsx
+++ src/pages/dashboard/requests/components/emptystate/index.tsx
@@ -6,7 +6,7 @@ import Loader from "@/components/custom/Loader"
 import { useNavigate } from "react-router-dom"
 
 
-export default function EmptyRequests() {
+export default function EmptyRequests({ activeTab = "request table" }: { activeTab?: string }) {
     const [ loading, setLoading ] = useState(false);
     const navigate = useNavigate();
 
@@ -29,6 +29,7 @@ export default function EmptyRequests() {
                     <h1 className="text-[1.625rem] leading-8 font-bold">Research ethics approval made easy</h1>
                     <p>Enjoy efficiency, transparency, and accountability while eliminating material losses and communication delays in the review process</p>
                     <Button onClick={() => {handleFunc()}} className="flex gap-4 items-center justify-center py-3 px-6 w-full max-w-[16rem] mx-auto"><span><GoogleDoc /></span>Request Ethical Approval</Button>
+                    {activeTab === "review table" && <Button onClick={() => {handleFunc()}} className="flex gap-4 items-center justify-center py-3 px-6 w-full max-w-[16rem] mx-auto"><span><GoogleDoc /></span>View Submitted Requests</Button>}
                 </div>
             </div>
         </>   
