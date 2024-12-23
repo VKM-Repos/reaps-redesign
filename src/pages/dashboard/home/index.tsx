38ed7b33f1d4bb1196e9cc4a9f742a6a826343b2
Susu-spec <suleimansuwaibat@gmail.com>
2024-12-23 13:15:01 +0100

    Add fetch requests to home page

--- src/pages/dashboard/home/index.tsx
+++ src/pages/dashboard/home/index.tsx
@@ -3,12 +3,13 @@ import GoogleDoc from "@/components/custom/Icons/GoogleDoc";
 
 import { useNavigate } from "react-router-dom";
 import AssignedReuqustCard from "./reviewer/ReviewersDashboard";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Loader from "@/components/custom/Loader";
 import ResearcherHomePage from "./researcher";
 import InstitutionAdminHome from "./institution";
 import useUserStore from "@/store/user-store";
 import SuperAdminHome from "./super-admin";
+import { useGET } from "@/hooks/useGET.hook";
 
 const Home = () => {
   // create handle open function
@@ -25,13 +26,74 @@ const Home = () => {
     }, 5000);
   };
 
+  const fetchUrls: Record<string, string> = {
+    submitted: `requests/users/me?sort_direction=asc&skip=0&limit=100&status=Submitted`,
+    approved: `requests/users/me?sort_direction=asc&skip=0&limit=100&status=Approved`,
+    pending: `transactions`,
+    reviews: `reviews/reviewer`,
+  };
+
+
+   const { data: submitted, refetch: refetch_submitted } = useGET({
+          url: fetchUrls.submitted,
+          queryKey: ["FETCH_SUBMITTED_REQUESTS"],
+          // enabled: !!tabFetchUrls[],
+        });
+
+    const { data: approved, refetch: refetch_approved } = useGET({
+      url: fetchUrls.approved,
+      queryKey: ["FETCH_APPROVED_REQUESTS"]
+    })
+
+    const { data: pending, refetch: refetch_pending } = useGET({
+      url: fetchUrls.pending,
+      queryKey: ["FETCH_PENDING_TRANSACTIONS"]
+    })
+
+    const { data: reviews, refetch: refetch_reviews } = useGET({
+      url: fetchUrls.reviews,
+      queryKey: ["FECTH_REVIEWS"]
+    })
+  
+      // const { data: transactions, refetch: refetch_transactions } = useGET({
+      //     url: "transactions",
+      //     queryKey: ["FETCH_TRANSACTIONS"],
+  
+      // }) 
+
+      // const { data: }
+  
+        const reviewed_requests = (
+          reviews && 
+              reviews?.items.filter((item: any) => (
+                  item.status ? "Unsatifactory" : "Satisfactory"
+              )) || []
+        )
+  
+        const pending_transactions = (
+          pending && 
+              pending?.items.filter((item: any) => (
+                  item.status === "PENDING"
+              )) || []
+        )
+
+       
+  
+        useEffect(() => {
+          refetch_reviews();
+          refetch_pending();
+          refetch_approved();
+          refetch_submitted();
+        }, [])
+  
+
 
   return (
     <>
       {loading && <Loader />}
       <div className="flex flex-col gap-10">
         {activeRole === "reviewer" && (
-          <AssignedReuqustCard name={user?.first_name ?? ""} reviewed="37" pending="5" />
+          <AssignedReuqustCard name={user?.first_name ?? ""} reviewed={reviewed_requests} pending={pending_transactions} />
         )}
         <div className="flex flex-col gap-6">
           <div className="flex flex-col md:flex-row gap-5 md:gap-auto justify-between md:items-center mx-auto w-full">
