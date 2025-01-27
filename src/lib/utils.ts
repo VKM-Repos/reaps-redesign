26c73d8d6b356dfb1a95a2414596b31341061e8e
salissadiq <salissadiq@gmail.com>
2025-01-27 14:53:29 +0100

    feat(truncate-string): add custom function that truncate string

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -46,20 +46,27 @@ export function mapStatus(status: string) {
 
 export const statusColorMap: { [key: string]: { bg: string; text: string } } = {
   "Not Submitted Yet": { bg: "#FFEB99", text: "#705C00" },
-  Submitted: { bg: "#D6F8C7", text: "#1A3E1E" },
+  "Submitted": { bg: "#D6F8C7", text: "#1A3E1E" },
   "Awaiting Remita Upload": { bg: "#FFF4D1", text: "#A67B00" },
   "Awaiting Remita Confirmation": { bg: "#FFF4D1", text: "#A67B00" },
   "Awaiting Payment Confirmation": { bg: "#FFF4D1", text: "#A67B00" },
-  Waiver: { bg: "#B9E6FF", text: "#084D71" },
+  "Waiver": { bg: "#B9E6FF", text: "#084D71" },
   "Payment Confirmed": { bg: "#D6F8C7", text: "#1A3E1E" },
   "Payment Declined": { bg: "#FC8D94", text: "#320104" },
   "Review in Progress": { bg: "#FFEB99", text: "#705C00" },
   "Re Opened": { bg: "#FFD3B6", text: "#8C3D00" },
-  Approved: { bg: "#80EF80", text: "#0E1A0E" },
-  Declined: { bg: "#FC8D94", text: "#320104" },
+  "Approved": { bg: "#80EF80", text: "#0E1A0E" },
+  "Declined": { bg: "#FC8D94", text: "#320104" },
   "More Information Needed": { bg: "#FFD3B6", text: "#8C3D00" },
-  Satisfactory: { bg: "#D6F8C7", text: "#1A3E1E" },
-  Unsatisfactory: { bg: "#FC8D94", text: "#320104" },
+  "Satisfactory": { bg: "#D6F8C7", text: "#1A3E1E" },
+  "Unsatisfactory": { bg: "#FC8D94", text: "#320104" },
   "Not Yet Reviewed": { bg: "#FFEB99", text: "#705C00" },
-  Closed: { bg: "#B3B3B3", text: "#2C2C2C" },
+  "Closed": { bg: "#B3B3B3", text: "#2C2C2C" },
 };
+
+export function truncateString(str:string, maxLength:number, suffix = "...") {
+  if (str.length <= maxLength) {
+    return str; // No truncation needed
+  }
+  return str.slice(0, maxLength) + suffix;
+}
