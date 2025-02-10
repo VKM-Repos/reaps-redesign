fd33bf515d79b5d10dd5296295655c16a8096df4
salissadiq <salissadiq@gmail.com>
2025-02-10 13:00:52 +0100

    refactor(input): add additional styling for hiding date input

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,27 +1,26 @@
-import * as React from "react"
+import * as React from "react";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
-    
     return (
       <input
         autoComplete="off"
         type={type}
         className={cn(
-          "flex h-10 w-full rounded border focus:outline-none border-input bg-background px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50",
+          "flex h-10 w-full rounded border focus:outline-none border-input bg-background px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50 !appearance-none",
           className
         )}
         ref={ref}
         {...props}
       />
-    )
+    );
   }
-)
-Input.displayName = "Input"
+);
+Input.displayName = "Input";
 
-export { Input }
+export { Input };
