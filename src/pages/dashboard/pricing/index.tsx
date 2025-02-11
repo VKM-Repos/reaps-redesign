731523812af803421bbabf16647bb2f882dbe1af
Jawn <jawnchuks@gmail.com>
2025-02-11 11:44:52 +0100

    removed transaction history from pricing page, modified layout

--- src/pages/dashboard/pricing/index.tsx
+++ src/pages/dashboard/pricing/index.tsx
@@ -1,37 +1,31 @@
 import CategoryTable from "./view-pricing";
 import { categories } from "../requests/components/ethical-request-approval/data/categories";
 import { useNavigate } from "react-router-dom";
+import { TransitionElement } from "@/lib/transitions";
+import PageTitle from "@/components/custom/PageTitle";
+import TableWrapper from "@/components/custom/TableWrapper";
+import SearchGlobal from "@/components/custom/SearchGlobal";
+import { Loader } from "lucide-react";
 
 export default function Pricing() {
-  const navigate = useNavigate();
-
-  const handleFunc = () => {
-    navigate("/pricing/your-transactions");
-  };
+  const isPending = false;
 
   return (
     <>
-      <div className="flex flex-col gap-[1.5rem] mb-20">
-        <div className="flex flex-col gap-14 items-center">
-          <div className="flex flex-col gap-[1.25rem] mx-auto w-full">
-            <div className="flex flex-col md:flex-row gap-5 md:gap-auto justify-between md:items-center">
-              <h1 className="text-[1.875rem] font-bold">
-                <span>Pricing</span>
-              </h1>
-              <p onClick={handleFunc}>
-                <a className="text-sm text-[#192C8A]  font-semibold underline">
-                  View your transactions
-                </a>
-              </p>
-            </div>
-            <p className="text-sm text-[#454745]">
-              The pricing page helps you to get information on what you will be
-              charged based on your category as a researcher.
-            </p>
-          </div>
-          <CategoryTable categories={categories} />
+      <TransitionElement>
+        <div className="flex flex-col gap-[1.25rem]">
+          <PageTitle
+            title="Pricing"
+            actions={null}
+            description="The pricing page helps you to get information on what you will be
+        charged based on your category as a researcher."
+          />
+          <div className="mx-auto my-0 flex w-full flex-col items-center justify-center"></div>
+          <TableWrapper search={<SearchGlobal />} filter={null} actions={null}>
+            {isPending ? <Loader /> : <CategoryTable categories={categories} />}
+          </TableWrapper>
         </div>
-      </div>
+      </TransitionElement>
     </>
   );
 }
