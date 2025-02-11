1a4184110e5c6bc60e216b465981ea82ba39e429
Jawn <jawnchuks@gmail.com>
2025-02-11 11:45:17 +0100

    added GET hook to fetch transaction details by request id

--- src/pages/dashboard/pricing/view-transactions/TransactionDetails.tsx
+++ src/pages/dashboard/pricing/view-transactions/TransactionDetails.tsx
@@ -4,7 +4,6 @@ import {
   DialogTrigger,
   DialogContent,
   DialogClose,
-  DialogTitle,
   DialogDescription,
 } from "@/components/ui/dialog";
 import {
@@ -16,18 +15,19 @@ import {
 import HoverCancel from "@/components/custom/Icons/HoverCancel";
 import { Badge } from "@/components/ui/badge";
 import { statusColorMap } from "./transaction-table/columns";
-import { TransactionItem } from "@/types/transaction";
 import { usePATCH } from "@/hooks/usePATCH.hook";
 import { useToast } from "@/components/ui/use-toast";
-// import { queryClient } from "@/providers";
 import { useRef } from "react";
+import { useGET } from "@/hooks/useGET.hook";
+import Loader from "@/components/custom/Loader";
+import View from "@/components/custom/Icons/View";
+import { TransactionItem } from "@/types/transaction";
 
 type Props = {
-  transaction: TransactionItem;
-  refetch: () => void;
+  id: string;
 };
 
-export default function TransactionDetails({ transaction, refetch }: Props) {
+export default function TransactionDetails({ id }: Props) {
   const isMobile = useMediaQuery({ query: "(max-width: 767px)" });
   const close_dialog_ref = useRef<HTMLButtonElement | null>(null);
 
@@ -36,31 +36,36 @@ export default function TransactionDetails({ transaction, refetch }: Props) {
   };
   return isMobile ? (
     <Sheet>
-      <SheetTrigger className="text-sm text-[#192C8A]">View</SheetTrigger>
+      <SheetTrigger
+        className={`text-black flex justify-center items-center gap-2 ${
+          isMobile ? "p-2" : "p-3"
+        }`}
+      >
+        <View />
+        {isMobile ? null : <span>Transaction</span>}
+      </SheetTrigger>
       <SheetContent className="w-full h-full pt-[1.25rem] pb-[1.125rem] flex flex-col gap-4">
         <SheetClose className="absolute right-6 top-6 !w-fit mx-auto py-0 px-0 ml-4 flex items-center justify-start opacity-70 rounded-full hover:bg-[#14155E14] transition-opacity hover:opacity-100 focus:outline-none disabled:pointer-events-none">
           <HoverCancel />
         </SheetClose>
-        <TransactionDetailsContent
-          close_dialog={handle_close_dialog}
-          refetch={refetch}
-          transaction={transaction}
-        />
+        <TransactionDetailsContent close_dialog={handle_close_dialog} id={id} />
       </SheetContent>
     </Sheet>
   ) : (
     <Dialog>
-      <DialogTrigger className="text-sm text-[#192C8A]">View</DialogTrigger>
-      <DialogTitle></DialogTitle>
+      <DialogTrigger
+        className={`text-black flex justify-center items-center gap-2 ${
+          isMobile ? "p-2" : "p-3"
+        }`}
+      >
+        <View />
+        {isMobile ? null : <span>Transaction</span>}
+      </DialogTrigger>
       <DialogDescription className="hidden">
         Details of the selected transaction
       </DialogDescription>
       <DialogContent className="w-full max-w-[800px] h-full md:max-h-[650px] pt-[1.25rem] pb-[1.125rem] flex flex-col gap-4 rounded-[1.25rem]">
-        <TransactionDetailsContent
-          close_dialog={handle_close_dialog}
-          refetch={refetch}
-          transaction={transaction}
-        />
+        <TransactionDetailsContent close_dialog={handle_close_dialog} id={id} />
       </DialogContent>
       <DialogClose>
         <span ref={close_dialog_ref} className="hidden"></span>
@@ -70,20 +75,35 @@ export default function TransactionDetails({ transaction, refetch }: Props) {
 }
 
 function TransactionDetailsContent({
-  transaction,
-  refetch,
+  id,
   close_dialog,
 }: {
-  transaction: TransactionItem;
-  refetch: () => void;
+  id: string;
   close_dialog: () => void;
 }) {
   const { toast } = useToast();
 
+  const {
+    data,
+    isPending: isTransactionsLoading,
+    isError,
+    refetch,
+  } = useGET({
+    url: `transactions/by-request/${id}`,
+    queryKey: ["request_transaction"],
+  });
+
+  const transaction: TransactionItem = data;
+
+  const { mutate: requery, isPending: isRequerying } = usePATCH(
+    `transactions/${transaction?.transaction_reference}/re-query`,
+    { method: "PUT" }
+  );
+
   const formattedAmount = new Intl.NumberFormat("en-NG", {
     style: "currency",
     currency: "NGN",
-  }).format(transaction.amount);
+  }).format(transaction?.amount);
 
   const formattedDate = new Intl.DateTimeFormat("en-US", {
     year: "numeric",
@@ -93,12 +113,7 @@ function TransactionDetailsContent({
     minute: "2-digit",
     second: "2-digit",
     hour12: true,
-  }).format(new Date(transaction.request.created_at));
-
-  const { mutate: requery, isPending: isRequerying } = usePATCH(
-    `transactions/${transaction?.transaction_reference}/re-query`,
-    { method: "PUT" }
-  );
+  }).format(new Date(transaction?.request.created_at));
 
   const handleRequeryTransaction = () => {
     toast({
@@ -111,8 +126,6 @@ function TransactionDetailsContent({
       { self: "true" },
       {
         onSuccess: (data) => {
-          console.log("SUCCEESSSS");
-
           toast({
             title: "Re-query Successful",
             description: `Transaction status: ${data.status}`,
@@ -120,16 +133,11 @@ function TransactionDetailsContent({
           });
           refetch();
           close_dialog();
-          // queryClient.invalidateQueries({
-          //   predicate: (query) => query.queryKey.includes(["transaction"]),
-          // });
         },
         onError: (error) => {
-          console.log("ERRROR");
-
           const errorMessage =
             error?.response.data.detail ||
-            "Unable to re-query the transaction.";
+            "Unable to re-query the transaction?.";
           toast({
             title: "Re-query Failed",
             description: errorMessage,
@@ -139,8 +147,17 @@ function TransactionDetailsContent({
       }
     );
   };
+
   return (
     <>
+      {isTransactionsLoading && <Loader />}
+      {isError &&
+        toast({
+          title: "Something went wrong",
+          description: "cannot fetch transaction details",
+          variant: "destructive",
+        })}
+
       <div className="border-[#0E0F0C1F] border-b flex justify-between items-center text-[#040C21] w-full">
         <p className="pb-4 px-[1.125rem] font-semibold text-xl2">
           Transaction Details
@@ -152,17 +169,17 @@ function TransactionDetailsContent({
             <div className="flex flex-col gap-2 text-sm justify-center text-[#515152] w-full">
               <p className="text-bold font-semibold">Name of Researcher</p>
               <p className="text-[#868687]">
-                {transaction.request.user.first_name +
+                {transaction?.request.user.first_name +
                   " " +
-                  transaction.request.user.last_name}
+                  transaction?.request.user.last_name}
               </p>
             </div>
             <div className="flex flex-col gap-2 text-sm justify-center text-[#515152] w-full">
               <p className="text-bold font-semibold">Category</p>
               <p className="text-[#868687] capitalize">
-                {transaction.request.user.description ?? "Undergraduate"} -{" "}
+                {transaction?.request.user.description ?? "Undergraduate"} -{" "}
                 <strong>
-                  {transaction.request.user.education_level ?? "Researcher"}
+                  {transaction?.request.user.education_level ?? "Researcher"}
                 </strong>
               </p>
             </div>
@@ -178,19 +195,19 @@ function TransactionDetailsContent({
           </div>
           <div className="flex flex-col md:flex-row gap-3 md:gap-5 text-sm items-start md:items-center text-[#515152]">
             <p className="font-bold text-sm">Request ID:</p>
-            <p>{transaction.id ?? "---"}</p>
+            <p>{transaction?.id ?? "---"}</p>
           </div>
           <div className="flex flex-col md:flex-row gap-3 md:gap-5 text-sm items-start md:items-center text-[#515152]">
             <p className="font-bold text-sm">Research Topic:</p>
-            <p>{transaction.request.research_title ?? "---"}</p>
+            <p>{transaction?.request.research_title ?? "---"}</p>
           </div>
           <div className="flex flex-col md:flex-row gap-3 md:gap-5 text-sm items-start md:items-center text-[#515152]">
             <p className="font-bold text-sm">Transaction Reference:</p>
-            <p>{transaction.transaction_reference ?? "---"}</p>
+            <p>{transaction?.transaction_reference ?? "---"}</p>
           </div>
           <div className="flex flex-col md:flex-row gap-3 md:gap-5 text-sm items-start md:items-center text-[#515152]">
             <p className="font-bold text-sm">Remita Reference:</p>
-            <p>{transaction.rrr ?? "---"}</p>
+            <p>{transaction?.rrr ?? "---"}</p>
           </div>
 
           <div className="flex flex-col md:flex-row gap-3 md:gap-5 text-sm items-start md:items-center text-[#515152]">
@@ -203,20 +220,20 @@ function TransactionDetailsContent({
             <p className="font-bold">Status:</p>
             <Badge
               style={{
-                color: statusColorMap[transaction.status]?.text || "#000000",
+                color: statusColorMap[transaction?.status]?.text || "#000000",
                 backgroundColor:
-                  statusColorMap[transaction.status]?.bg || "#192C8A",
+                  statusColorMap[transaction?.status]?.bg || "#192C8A",
               }}
               className="flex gap-1 items-center justify-center py-1 px-2 rounded-[2.25rem] font-[400]"
             >
               <div
                 style={{
                   backgroundColor:
-                    statusColorMap[transaction.status]?.text || "#192C8A",
+                    statusColorMap[transaction?.status]?.text || "#192C8A",
                 }}
                 className="w-[5px] h-[5px] rounded-full "
               ></div>
-              {transaction.status.toLowerCase() ?? "---"}
+              {transaction?.status.toLowerCase() ?? "---"}
             </Badge>
           </div>
         </div>
