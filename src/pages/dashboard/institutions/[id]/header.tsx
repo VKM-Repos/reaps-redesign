c944d2502619ea26481d11793c0518f6cf4a23e2
Jawn <jawnchuks@gmail.com>
2025-02-13 19:07:30 +0100

    created header layout for institution details pages

---
+++ src/pages/dashboard/institutions/[id]/header.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+type Props = {
+  title: string;
+  actions?: React.ReactNode;
+};
+
+const Header = ({ title, actions }: Props) => {
+  return (
+    <nav className="w-full border-b flex items-center justify-between py-4">
+      <h3 className="text-black text-xl font-semibold">{title}</h3>
+      <div>{actions}</div>
+    </nav>
+  );
+};
+
+export default Header;
