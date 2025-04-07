3216b823b938a8a0fd18934e3eda323edb9803cc
Susu-spec <suleimansuwaibat@gmail.com>
2025-04-07 15:29:16 +0100

    restructure charts components

--- src/pages/dashboard/institution/finance/charts/data.ts
+++ src/pages/dashboard/institution/finance/charts/data.ts
@@ -111,3 +111,22 @@ export const paymentsData = [
   
     ]
   
+export const revenueComparisonData = [
+    { month: 'January', this_year: 40, last_year: 90 },
+    { month: 'February', this_year: 80, last_year: 30 },
+    { month: 'March', this_year: 150, last_year: 90 },
+    { month: 'April', this_year: 98, last_year: 90 },
+    { month: 'May', this_year: 110, last_year: 90 },
+    { month: 'June', this_year: 40, last_year: 50 },
+    { month: 'July', this_year: 40, last_year: 70 },
+    { month: 'August', this_year: 80, last_year: 90 },
+    { month: 'September', this_year: 40, last_year: 90 },
+    { month: 'October', this_year: 100, last_year: 90 },
+    { month: 'November', this_year: 80, last_year: 90 },
+    { month: 'December', this_year: 60, last_year: 90 },
+]
+
+export const paymentsGenderData = [
+    { gender: 'female', submissions: 120, fill: '#DE3B40' },
+    { gender: 'male', submissions: 86, fill: '#192C8A' }
+]
---
+++ src/pages/dashboard/institution/finance/charts/index.tsx
@@ -0,0 +1,169 @@
+import { Bar, BarChart, CartesianGrid, Label, Pie, PieChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
+import { MonthlyPaymentTooltip, RevenueTooltip } from "./tooltips";
+import { paymentsData, paymentsGenderData, revenueComparisonData, revenueData } from "./data";
+import { CurvedBar, SplitCurvedBar } from "./custom-bars";
+import { 
+    ChartContainer,
+    ChartTooltip,
+    ChartTooltipContent 
+} from "@/components/ui/chart";
+import { useMemo } from "react";
+import { payment_by_gender_config, revenue_comparison_config } from "./configs";
+
+export function MonthlyPaymentsChart() {
+    return (
+        <ResponsiveContainer width="100%" height={330}>
+            <BarChart data={paymentsData}  barCategoryGap="20%" barGap={4} margin={{ top: 10, left: -25, right: 30, bottom: 20 }}>
+            <CartesianGrid horizontal={true} vertical={false} strokeDasharray="10 10" />
+            <XAxis
+                dataKey="month"
+                tick={{ fill: '#333', fontSize: 12 }}
+                tickLine={false}
+                axisLine={false}
+                dy={20}
+            />
+            <YAxis
+                dataKey="earning.valueA"
+                tick={{ fill: '#333', fontSize: 12 }}
+                axisLine={false}
+                tickLine={false}
+                tickFormatter={(value) => (value >= 1000 ? `${value / 1000}k` : value)}
+            />
+            <Tooltip content={<MonthlyPaymentTooltip />} cursor={false}/>
+            <Bar
+                dataKey="earning.valueA" 
+                shape={<SplitCurvedBar />}
+                barSize={7.5}
+                isAnimationActive={true}
+            />
+        </BarChart>
+      </ResponsiveContainer>
+    
+    )
+  }
+
+
+
+export function RevenueCategoryChart() {
+    return (
+        <ResponsiveContainer width='100%' height={330}>
+            <BarChart data={revenueData} barCategoryGap='10%' barGap={1} layout="vertical">
+                <CartesianGrid horizontal={false} vertical={true} strokeDasharray="10 10" />
+                <XAxis 
+                    type="number"
+                    tick={{ fill: '#333', fontSize: 12 }}
+                    tickLine={false}
+                    axisLine={false}
+                    orientation="bottom"
+                    domain={[0, 1500]}
+                    ticks={[0, 500, 1000, 1500]}
+                />
+
+                <YAxis 
+                    type="category"
+                    dataKey='category'
+                    tick={{ fill: '#333', fontSize: 10, fontWeight: 600, textAnchor: 'start', dx: -55, }}
+                    axisLine={false}
+                    tickLine={false}
+                    orientation="left"
+                    dx={-5}
+                />
+                <Tooltip content={<RevenueTooltip />} cursor={false}/>
+                <Bar 
+                    dataKey='revenue'
+                    shape={<CurvedBar />}
+                    barSize={14}
+                />
+                </BarChart>
+        </ResponsiveContainer>
+    )
+}
+
+export function RevenueComparisonChart() {
+    return (
+        <ChartContainer config={revenue_comparison_config} className="w-full min-h-[12.5rem]">
+              <BarChart accessibilityLayer 
+                data={revenueComparisonData} 
+                barCategoryGap={10} 
+                barGap={2}
+                margin={{ left: -30 }}>
+                <CartesianGrid horizontal={true} vertical={false} strokeDasharray="4 4" stroke="#E5E7EB"/>
+                <XAxis
+                    dataKey="month"
+                    tickLine={false}
+                    tickMargin={10}
+                    axisLine={false}
+                    tickFormatter={(value) => value.slice(0, 3)}
+                />
+                <YAxis 
+                    tick={{ fill: '#333', fontSize: 10, textAnchor: 'start', dx: -15, }}
+                    axisLine={false}
+                    tickLine={false}
+                    orientation="left"
+                />
+                <Bar dataKey='this_year' fill="#192C8A" radius={6} />
+                <Bar dataKey='last_year' fill="#D1D5DB" radius={6}/>
+            </BarChart>
+        </ChartContainer>
+    )
+}
+
+
+export function PaymentByGenderChart() {
+
+    const totalSubmissions = useMemo(() => {
+        return paymentsGenderData.reduce((acc, curr) => acc + curr.submissions, 0)
+      }, [])
+
+
+    return (
+        <ChartContainer config={payment_by_gender_config} className="mx-auto aspect-square max-h-[250px]">
+            <PieChart>
+                <ChartTooltip
+                    cursor={false}
+                    content={<ChartTooltipContent hideLabel />}
+                />
+                <Pie data={paymentsGenderData} 
+                    dataKey='submissions' 
+                    nameKey='gender' 
+                    innerRadius={80}
+                    outerRadius={100}
+                    strokeWidth={2}>
+                        <Label
+                            content={({ viewBox }: any) => {
+                            if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                                return ( 
+                                <text
+                                    x={viewBox.cx}
+                                    y={viewBox.cy}
+                                    textAnchor="middle"
+                                    dominantBaseline="middle"
+                                    >
+                                    <tspan
+                                        x={viewBox.cx}
+                                        y={viewBox.cy - 20}
+                                        fill="#6B7280"
+                                        style={{ fontSize: '14px', fontWeight: 600 }}
+                                    >
+                                        Total
+                                    </tspan>
+
+                                    <tspan
+                                        x={viewBox.cx}
+                                        y={(viewBox.cy || 0) + 20}
+                                        fill="#111827"
+                                        style={{ fontSize: '24px', fontWeight: 'bold' }}
+                                    >
+                                        {totalSubmissions.toLocaleString()}
+                                    </tspan>
+                                    </text>
+
+                                )
+                            }
+                            }}
+                        />
+                    </Pie>
+            </PieChart>
+        </ChartContainer>
+    )
+}
