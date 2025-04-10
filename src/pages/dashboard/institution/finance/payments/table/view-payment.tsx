6fc1acb630be31df803092f7c886c76c9a86a518
Susu-spec <suleimansuwaibat@gmail.com>
2025-04-10 10:49:13 +0100

    feat(payments-section): add  dummy content to payment details

--- src/pages/dashboard/institution/finance/payments/table/view-payment.tsx
+++ src/pages/dashboard/institution/finance/payments/table/view-payment.tsx
@@ -1,5 +1,6 @@
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Sheet, SheetClose, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import ViewIcon from '@/assets/view.svg'
+import { X } from "lucide-react";
 
 
 export default function ViewPayment() {
@@ -10,9 +11,10 @@ export default function ViewPayment() {
                 <span className="flex gap-3 items-center justify-center text-[#333740] text-sm">View <img src={ViewIcon} /></span>
             </SheetTrigger>
             <SheetContent 
-                side='bottom' className={`fixed inset-x-auto inset-y-0
-                 w-full max-w-[800px] h-full md:max-h-[650px] pt-[1.25rem] pb-[1.125rem] flex flex-col gap-4 rounded-[1.25rem]`}>
-                
+                side='top' 
+                className={`fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full max-w-[800px] h-full md:max-h-[650px]  pt-[1.25rem] pb-[1.125rem] flex flex-col gap-4 rounded-[1.25rem]`}>
+            <SheetClose className="w-full flex justify-end px-5"><X /></SheetClose>
+                Payment Details
             </SheetContent>
         </Sheet>
     )
